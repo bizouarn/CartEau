2552cb7416f3d2e0f203118ac624b0fa67e27be9
Aymeric Bizouarn <aymeric.bizouarn@hotmail.fr>
2022-04-19 23:42:52 +0200

    Generate data for water_quality

--- data/water_quality.js
+++ data/water_quality.js
@@ -1,17 +1,28 @@
 const axios = require('axios')
+const { trace } = require('console')
+const fs = require('fs')
 
 class dataWaterQuality{
     constructor(){
         
     }
     async getPlaces(page){
-        var url = 'https://hubeau.eaufrance.fr/api/vbeta/qualite_eau_potable/communes_udi?size=100'
+        var communes_udi = []
+        // read json file laposte_hexasmal.json
+        fs.readFile('./private/laposte_hexasmal.json', 'utf8', function (err, data) {
+            var data = JSON.parse(data)
+            for(var communes in data){
+                communes_udi[data[communes].fields.code_commune_insee] = data[communes].fields;
+            }
+        })
+        var size = 1000;
+        var url = 'https://hubeau.eaufrance.fr/api/vbeta/qualite_eau_potable/communes_udi?size='+size
         var places = await axios.get(url+'&page='+page).then(
             async function(response){ 
                 if(response.status == 206){
                     var ret = []
                     var count = response.data.count;
-                    var nbPage = Math.ceil(count/100);
+                    var nbPage = Math.ceil(count/size);
                     console.log('Warning : We have to get more data. nbPage:'+nbPage);
                     for(var i = 1; i <= nbPage ; i++){
                         console.log("page",i)
@@ -21,6 +32,8 @@ class dataWaterQuality{
                             }
                         )
                         for(var place of tmp){
+                            place.x = communes_udi[place.code_commune].coordonnees_gps[1]
+                            place.y = communes_udi[place.code_commune].coordonnees_gps[0]
                             ret.push(place)
                         }
                         console.log('nb places',ret.length,tmp.length)
@@ -37,8 +50,7 @@ class dataWaterQuality{
         return places;
     }        
     async getData(value,callback){
-        var places = []
-        //var places = await this.getPlaces(1)
+        var places = await this.getPlaces(1)
         callback(places, value)
     }
     getInfo(code){
