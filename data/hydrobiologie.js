e126be23883cba271d99b6e74e1e3c08eeb4b8b1
Aymeric Bizouarn <aymeric.bizouarn@hotmail.fr>
2023-03-01 19:22:14 +0100

    Generate JSON for Hydrobiologie 🪲

---
+++ data/hydrobiologie.js
@@ -0,0 +1,51 @@
+const axios = require('axios')
+
+class dataFish{
+    constructor(){
+        
+    }    
+    async getPlaces(page){
+        var places = [];
+        var url = "https://hubeau.eaufrance.fr/api/v1/hydrobio/stations_hydrobio?format=json&size=2000";
+        console.log(url)
+        var tmp = await axios.get(url).then(
+            async function(response){ 
+                try{
+                    if(response.status == 200 || response.status == 206){
+                        var count = response.data.count;
+                        var nbPage = Math.ceil(count/2000);
+                        for(var i = 1; i <= nbPage ; i++){
+                            console.log("page",i)
+                            var tmp = await axios.get(url+'&page='+i).then(
+                                async function(response){ 
+                                    return response.data.data;
+                                }
+                            )
+                            for(var place of tmp){
+                                places.push(place)
+                            }
+                            console.log('nb places',places.length,tmp.length)
+                        }
+                    } else {
+                        return response.data.data;
+                    }
+                }catch(err){
+                    console.log(err)
+                }
+            }
+        )
+        for(var place of places){
+            place.x = place.longitude;
+            place.y = place.latitude;
+        }
+        return places;
+    }
+    async getData(value,callback){
+        var places = await this.getPlaces(1)
+        callback(places, value)
+    }
+    getInfo(code){
+        // TODO
+    }
+}
+module.exports = dataFish;
